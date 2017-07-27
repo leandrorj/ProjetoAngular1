a7dcbe87471069968f49601b92eabf5ff5e1708f
Leandro I. Farias <leandro.i.farias@avanade.com>
2017-07-27 11:30:12 -0300

    criacao da opcao de grupos

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -16,4 +16,36 @@ ddo.templateUrl = 'js/directives/meu-painel.html';
 
 
 	return ddo;
+})
+.directive('minhaFoto', function(){
+	var ddo = {};
+
+	ddo.restric = "AE";
+
+	ddo.scope = {
+		titulo: '@',
+		url: '@'
+	};
+	ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';
+
+	return ddo;
+
+})
+.directive('meuBotaoPerigo', function(){
+	var ddo = {};
+	ddo.restrict = "E";
+
+	ddo.scope={
+		
+		nome: '@', // @ - é uma copia do valor - serve para passar string
+		acao: '&', // & serve para passar uma expressao
+
+	};
+
+	ddo.template = '<button ng-click="acao(foto)" class="btn btn-danger btn-block">{{nome}}</button>';
+
+	return ddo;
+
+
 });
+
