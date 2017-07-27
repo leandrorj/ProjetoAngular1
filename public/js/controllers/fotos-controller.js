84463150efbdd5fa70c0a6e587f116d485c441c0
Leandro I. Farias <leandro.i.farias@avanade.com>
2017-07-27 09:46:06 -0300

    criacao do botao remover funcionando

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -4,6 +4,7 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 
 	$scope.fotos = [];
 	$scope.filtro = '';
+	$scope.mensagem = '';
 
 	//utilizando de forma simplificada
 	$http.get('v1/fotos')
@@ -14,6 +15,24 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 		console.log(erro);
 	})
 
+
+	$scope.remover = function(foto){
+		$http.delete('v1/fotos/' + foto._id)
+		.success(function(){
+			
+			//atualiza a lista (sem fazer uma nova requisicao) apos a remoçao da foto
+			var indeceFoto = $scope.fotos.indexOf(foto);
+			$scope.fotos.splice(indeceFoto, 1);
+			
+			$scope.mensagem = 'Foto ' + foto.titulo + 'removido com sucesso!';
+		})
+		.error(function(erro){
+		console.log(erro);
+		$scope.mensagem = 'Não foi possivel remover a foto ' + foto.titulo;
+	})
+
+	};
+});
 /* utilizando json
 
 		var promise =  $http.get('v1/fotos');
@@ -44,4 +63,3 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 				url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
 			},
 		] */
-});
